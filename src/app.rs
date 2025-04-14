1a86a4ca9ff17c7d80f4c4502f9399b71b7f3ba6
Ztirom45 <moritz.busse.mb@web.de>
2025-04-14 21:54:49 +0200

    add iResolution and make window resizeble

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,22 @@
+use std::ffi::CString;
+
 use beryllium::{init::InitFlags, video::GlWindow, *};
 
 use ogl33::*;
-use crate::shader::*;
+use crate::shader::{self, *};
 
 pub struct App{
   pub sdl:Sdl,
   pub win:GlWindow,
-  pub shader:Shader,
+  pub shader_program:u32,
 }
+
+ 
+type Vertex = [f32; 3];
+const VERTICES: [Vertex; 3] =
+    [[-0.5, -0.5, 0.0], [0.5, -0.5, 0.0], [0.0, 0.5, 0.0]];
+    
+
 
 impl App{
   pub fn new() -> Self{
@@ -29,7 +38,7 @@ impl App{
         height: 600,
         allow_high_dpi: true,
         borderless: false,
-        resizable: false,
+        resizable: true,
       };
 
     let win = sdl
@@ -39,49 +48,51 @@ impl App{
       ogl33::glEnable(ogl33::GL_TEXTURE_2D);
     }*/
     //creat mesh
-    
-    type Vertex = [f32; 3];
-    const VERTICES: [Vertex; 3] =
-    [[-0.5, -0.5, 0.0], [0.5, -0.5, 0.0], [0.0, 0.5, 0.0]];
+    let shader_program:u32;
     unsafe{ 
-      load_gl_with(|f_name| win.get_proc_address(f_name as *const u8));
+      load_gl_with(|f_name| win.get_proc_address(f_name.cast()));
+
+      glClearColor(0.2, 0.3, 0.3, 1.0);
+
       let mut vao = 0;
       glGenVertexArrays(1, &mut vao);
       assert_ne!(vao, 0);
-      
-      let mut vbo = 0;
-      glGenBuffers(1, &mut vbo);
-      assert_ne!(vbo, 0);
-      glBindBuffer(GL_ARRAY_BUFFER, vbo);
-      glBufferData(
-        GL_ARRAY_BUFFER,
-        size_of_val(&VERTICES) as isize,
-        VERTICES.as_ptr().cast(),
-        GL_STATIC_DRAW,
-      );
-      glVertexAttribPointer(
-        0,
-        3,
-        GL_FLOAT,
-        GL_FALSE,
-        size_of::<Vertex>().try_into().unwrap(),
-        0 as *const _,
-      );
+      glBindVertexArray(vao);
       glEnableVertexAttribArray(0);
+      shader_program = make_shader();
     }
-    let shader = Shader::new(&win);
+    //let shader = Shader::new(&win);
     return Self{
       sdl,
       win,
-      shader
+      shader_program,
     };
   }
 
-
+  pub unsafe fn update_uniforms(&mut self){
+        let i_resolution = self.win.get_window_size();
+        println!("{} {}",i_resolution.0,i_resolution.1);
+        
+        //update viewport
+        glViewport(0, 0, i_resolution.0*2, i_resolution.1*2);
+        
+        let i_resolution_location = glGetUniformLocation(
+        self.shader_program,
+        CString::new("iResolution").unwrap().as_ptr() as *const i8
+        );
+	if i_resolution_location>=0{
+		glUniform2f(i_resolution_location,i_resolution.0 as f32,i_resolution.1 as f32);
+	}else{
+		panic!("error: couldn't find view uniform\n");
+		//exit(EXIT_FAILURE);
+	}
+   
+  }
   pub fn pre_draw(&mut self){
     unsafe {
       load_gl_with(|f_name| self.win.get_proc_address(f_name as *const u8));
-      glClearColor(0.2, 0.3, 0.3, 1.0);
+      self.update_uniforms();
+      //glClearColor(0.2, 0.3, 0.3, 1.0);
       glClear(GL_COLOR_BUFFER_BIT);
     }
 
@@ -100,17 +111,16 @@ impl App{
   }*/
   pub fn draw(&mut self){
     unsafe{
-      glDrawArrays(GL_TRIANGLES, 0, 3);
-      //glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+      glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
     }
   }
   pub fn update(&mut self){ 
     self.pre_draw();
     self.draw();
-    self.win.swap_window();   
+    self.win.swap_window();
   }
 
   pub fn clear(&mut self){
-    self.shader.clear(&self.win);
+    //self.shader.clear(&self.win);
   }
 }
